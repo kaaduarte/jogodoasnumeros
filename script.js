621845dce282a7c93d630ec66dd4c70e9790a854
kaaduarte <programacaoduarte@gmail.com>
2024-10-07 10:26:03 -0300

    função

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function gerarNumeroAleatorio() {
     return new Promise((resolve) =>{
         setTimeout(()=>{
             numeroSecreto = Math.floor(Math.random()*100)+1;
-        })
-    }
-    )
+            resolve(numeroSecreto);
+        },1000);
+    });
 }
